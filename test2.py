a10ad08eeb212fe794e2fef70775f0b8a925856d
YangAo <a15303812991@163.com>
2025-11-29 14:04:49 +0800

    add test2

---
+++ test2.py
@@ -0,0 +1,51 @@
+import math
+import pyecharts
+import pyspark
+import socket
+
+
+id=123
+name1="Alice"
+name2="Bob" 
+age=17
+
+def fun(*args):
+    for i in args:
+        print(i)
+fun(id,age)
+# day="Monday"
+# print
+# try:
+#     result=day
+# except Exception as e:
+#     print("Error:",e)
+# finally:
+#     print("Finished attempting to access 'day' variable.")
+# with open("C:/Users/a1530/Desktop/a.txt","r",encoding="UTF-8") as f:
+#     content=f.read()
+#     print(content.count("a"))
+# list=[1,2,3]
+# list.append([4,5,6])
+# print(list)
+# dict={"Bob":20,"Alice":30}
+# print(max(dict))
+# fun=lambda x:x+1
+# print(fun(2))
+# def sub(x,y):
+#     return add(x,y)-2*y
+# def add(x,y):
+#     return x+y
+
+
+# set={"123","456","789"}
+# re=set.pop()
+# print(re)
+
+# def sayhi(x,y):
+#     global a
+#     a=20
+#     print(x+y)
+#     return x+y
+# sayhi(1,2)
+# if a >= 18:
+#     print(f"{name1} is an adult.")
